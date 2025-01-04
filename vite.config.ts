a811480c068a929b9499a0567eae9ab840967e6d
Justin <j.hebenstreit25@gmail.com>
2025-01-03 19:32:37 -0600

    fixed some errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    assetsInclude: ['**/*.md'],
-    build: {
-        rollupOptions: {
-            plugins: [
-                {
-                    name: 'load-md-files',
-                    transform(code, id) {
-                        if (id.endsWith('.md')) {
-                            return `export default ${JSON.stringify(code)}`;
-                        }
-                    },
-                },
-            ],
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
     },
+  },
 });
