6dd98ae6812d731836b4c2aba0a1832a165be2ff
Justin <j.hebenstreit25@gmail.com>
2025-01-05 23:01:19 -0600

    fixed navigation and index.tsx

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -39,40 +39,66 @@ const pages: Page[] = [
             ],
           },
           {
-            name: "LESS",
+            name: "Frameworks",
             subpages: [
-              { name: "LESS Notes", path: "/css/less" },
-              { name: "LESS Code Notes", path: "/css/less/lesscode" },
-            ],
-          },
-          {
-            name: "PostCSS",
-            subpages: [
-              { name: "PostCSS Notes", path: "/css/postcss" },
-              { name: "PostCSS Code Notes", path: "/css/postcss/postcsscode" },
-            ],
-          },
-          {
-            name: "SASS",
-            subpages: [
-              { name: "SASS Notes", path: "/sass" },
-              { name: "SASS Code Notes", path: "/sass/sasscode" },
-            ],
-          },
-          {
-            name: "SCSS",
-            subpages: [
-              { name: "SCSS Notes", path: "/scss" },
-              { name: "SCSS Code Notes", path: "/scss/scsscode" },
-            ],
-          },
-          {
-            name: "Tailwind CSS",
-            subpages: [
-              { name: "Tailwind CSS Notes", path: "/tailwindcss" },
               {
-                name: "Tailwind CSS Code Notes",
-                path: "/tailwindcss/tailwindcsscode",
+                name: "Bootstrap",
+                subpages: [
+                  { name: "Bootstrap Notes", path: "/css/frameworks/bootstrap" },
+                  { name: "Bootstrap Code Notes", path: "/css/frameworks/bootstrap/bootstrapcode" },
+                ],
+              },
+              {
+                name: "React Bootstrap",
+                subpages: [
+                  { name: "React Bootstrap Notes", path: "/css/frameworks/reactbootstrap" },
+                  { name: "React Bootstrap Code Notes", path: "/css/frameworks/reactbootstrap/reactbootstrapcode" },
+                ],
+              },
+              {
+                name: "Tailwind CSS",
+                subpages: [
+                  { name: "Tailwind CSS Notes", path: "/css/frameworks/tailwind" },
+                  { name: "Tailwind CSS Code Notes", path: "/css/frameworks/tailwind/tailwindcode" },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Preprocessors",
+            subpages: [
+              {
+                name: "LESS",
+                subpages: [
+                  { name: "LESS Notes", path: "/css/preprocessors/less" },
+                  { name: "LESS Code Notes", path: "/css/preprocessors/lesscode" },
+                ],
+              },
+              {
+                name: "SASS",
+                subpages: [
+                  { name: "SASS Notes", path: "/css/preprocessors/sass" },
+                  { name: "SASS Code Notes", path: "/css/preprocessors/sasscode" },
+                ],
+              },
+              {
+                name: "SCSS",
+                subpages: [
+                  { name: "SCSS Notes", path: "/css/preprocessors/scss" },
+                  { name: "SCSS Code Notes", path: "/css/preprocessors/scsscode" },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Post-Processors",
+            subpages: [
+              {
+                name: "PostCSS",
+                subpages: [
+                  { name: "PostCSS Notes", path: "/css/postprocessors/postcss" },
+                  { name: "PostCSS Code Notes", path: "/css/postprocessors/postcsscode" },
+                ],
               },
             ],
           },
@@ -166,6 +192,13 @@ const pages: Page[] = [
         ],
       },
       {
+        name: "Svelte",
+        subpages: [
+          { name: "Svelte Notes", path: "/svelte" },
+          { name: "Svelte Code Notes", path: "/svelte/sveltecode" },
+        ],
+      },
+      {
         name: "Vue",
         subpages: [
           { name: "Vue Notes", path: "/vue" },
@@ -185,55 +218,270 @@ const pages: Page[] = [
         ],
       },
       {
-        name: "MERN",
+        name: "Authentication and Authorization",
         subpages: [
-          { name: "MERN Notes", path: "/mern" },
-          { name: "MERN Code Notes", path: "/mern/merncode" },
+          { name: "Auth Notes", path: "/auth" },
+          { name: "Auth Code Notes", path: "/auth/authcode" },
         ],
       },
       {
-        name: "Node JS",
+        name: "Frameworks",
         subpages: [
-          { name: "Node JS Notes", path: "/node" },
-          { name: "Node JS Code Notes", path: "/node/nodecode" },
+          {
+            name: "Django",
+            subpages: [
+              { name: "Django Notes", path: "/django" },
+              { name: "Django Code Notes", path: "/django/djangocode" },
+            ],
+          },
+          {
+            name: "Express.js",
+            subpages: [
+              { name: "Express.js Notes", path: "/express" },
+              { name: "Express.js Code Notes", path: "/express/expresscode" },
+            ],
+          },
+          {
+            name: "Flask",
+            subpages: [
+              { name: "Flask Notes", path: "/flask" },
+              { name: "Flask Code Notes", path: "/flask/flaskcode" },
+            ],
+          },
+          {
+            name: "Koa.js",
+            subpages: [
+              { name: "Koa.js Notes", path: "/koa" },
+              { name: "Koa.js Code Notes", path: "/koa/koacode" },
+            ],
+          },
+          {
+            name: "MERN",
+            subpages: [
+              { name: "MERN Notes", path: "/mern" },
+              { name: "MERN Code Notes", path: "/mern/merncode" },
+            ],
+          },
+          {
+            name: "Node.js",
+            subpages: [
+              { name: "Node.js Notes", path: "/node" },
+              { name: "Node.js Code Notes", path: "/node/nodecode" },
+            ],
+          },
+          {
+            name: "Ruby",
+            subpages: [
+              { name: "Ruby Notes", path: "/ruby" },
+              { name: "Ruby Code Notes", path: "/ruby/rubycode" },
+              {
+                name: "Ruby on Rails",
+                subpages: [
+                  { name: "Ruby on Rails Notes", path: "/ruby/rubyonrails" },
+                  { name: "Ruby on Rails Code Notes", path: "/ruby/rubyonrails/rubyonrailscode" },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Spring Boot",
+            subpages: [
+              { name: "Spring Boot Notes", path: "/springboot" },
+              { name: "Spring Boot Code Notes", path: "/springboot/springbootcode" },
+            ],
+          },
+          {
+            name: "WebSockets",
+            subpages: [
+              { name: "WebSockets Notes", path: "/websockets" },
+              { name: "WebSockets Code Notes", path: "/websockets/websocketscode" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "GraphQL & Apollo",
+        subpages: [
+          {
+            name: "GraphQL",
+            subpages: [
+              { name: "GraphQL Notes", path: "/graphql" },
+              { name: "GraphQL Code Notes", path: "/graphql/graphqlcode" },
+            ],
+          },
+          {
+            name: "Apollo Server",
+            subpages: [
+              { name: "Apollo Server Notes", path: "/apolloserver" },
+              { name: "Apollo Server Code Notes", path: "/apolloserver/apolloservercode" },
+            ],
+          },
         ],
       },
       {
         name: "Servers",
         subpages: [
-          { name: "General Server Notes", path: "/servers" },
-          { name: "General Server Code Notes", path: "/servers/servercode" },
+          {
+            name: "General Server Concepts",
+            subpages: [
+              { name: "General Server Notes", path: "/servers/general" },
+              { name: "General Server Code Notes", path: "/servers/general/servercode" },
+            ],
+          },
+          {
+            name: "Nginx",
+            subpages: [
+              { name: "Nginx Notes", path: "/servers/nginx" },
+              { name: "Nginx Code Notes", path: "/servers/nginx/nginxcode" },
+            ],
+          },
+          {
+            name: "Apache",
+            subpages: [
+              { name: "Apache Notes", path: "/servers/apache" },
+              { name: "Apache Code Notes", path: "/servers/apache/apachecode" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Databases",
+        subpages: [
+          {
+            name: "Firebase",
+            subpages: [
+              { name: "Firebase Notes", path: "/databases/firebase" },
+              { name: "Firebase Code Notes", path: "/databases/firebase/firebasecode" },
+            ],
+          },
+          {
+            name: "MongoDB",
+            subpages: [
+              { name: "MongoDB Notes", path: "/databases/mongodb" },
+              { name: "MongoDB Code Notes", path: "/databases/mongodb/mongodbcode" },
+            ],
+          },
+          {
+            name: "MySQL",
+            subpages: [
+              { name: "MySQL Notes", path: "/databases/mysql" },
+              { name: "MySQL Code Notes", path: "/databases/mysql/mysqlcode" },
+            ],
+          },
+          {
+            name: "PostgreSQL",
+            subpages: [
+              { name: "PostgreSQL Notes", path: "/databases/postgresql" },
+              { name: "PostgreSQL Code Notes", path: "/databases/postgresql/postgresqlcode" },
+            ],
+          },
+          {
+            name: "Redis",
+            subpages: [
+              { name: "Redis Notes", path: "/databases/redis" },
+              { name: "Redis Code Notes", path: "/databases/redis/rediscode" },
+            ],
+          },
+          {
+            name: "SQLite",
+            subpages: [
+              { name: "SQLite Notes", path: "/databases/sqlite" },
+              { name: "SQLite Code Notes", path: "/databases/sqlite/sqlitecode" },
+            ],
+          },
         ],
       },
     ],
-  },
+  },  
   {
-    name: "Databases",
+    name: "Tools",
     subpages: [
       {
-        name: "MongoDB",
+        name: "API Tools",
         subpages: [
-          { name: "MongoDB Notes", path: "/mongodb" },
-          { name: "MongoDB Code Notes", path: "/mongodb/mongodbcode" },
+          {
+            name: "Insomnia",
+            subpages: [
+              { name: "Insomnia Notes", path: "/tools/api/insomnia" },
+              { name: "Insomnia Code Notes", path: "/tools/api/insomnia/insomniacode" },
+            ],
+          },
+          {
+            name: "Postman",
+            subpages: [
+              { name: "Postman Notes", path: "/tools/api/postman" },
+              { name: "Postman Code Notes", path: "/tools/api/postman/postmancode" },
+            ],
+          },
         ],
       },
       {
-        name: "PostgreSQL",
+        name: "CI/CD Tools",
         subpages: [
-          { name: "PostgreSQL Notes", path: "/postgresql" },
-          { name: "PostgreSQL Code Notes", path: "/postgresql/postgresqlcode" },
+          {
+            name: "CircleCI",
+            subpages: [
+              { name: "CircleCI Notes", path: "/tools/cicd/circleci" },
+              { name: "CircleCI Code Notes", path: "/tools/cicd/circleci/circlecicode" },
+            ],
+          },
+          {
+            name: "Jenkins",
+            subpages: [
+              { name: "Jenkins Notes", path: "/tools/cicd/jenkins" },
+              { name: "Jenkins Code Notes", path: "/tools/cicd/jenkins/jenkinscode" },
+            ],
+          },
         ],
       },
-    ],
-  },
-  {
-    name: "Tools & Testing",
-    subpages: [
       {
-        name: "CI/CD Ops",
+        name: "Code Quality",
         subpages: [
-          { name: "CI/CD Ops Notes", path: "/cicdops" },
-          { name: "CI/CD Ops Code Notes", path: "/cicdops/cicdopscode" },
+          {
+            name: "ESLint",
+            subpages: [
+              { name: "ESLint Notes", path: "/tools/codequality/eslint" },
+              { name: "ESLint Code Notes", path: "/tools/codequality/eslintcode" },
+            ],
+          },
+          {
+            name: "Prettier",
+            subpages: [
+              { name: "Prettier Notes", path: "/tools/codequality/prettier" },
+              { name: "Prettier Code Notes", path: "/tools/codequality/prettiercode" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Package Management & Bundling",
+        subpages: [
+          {
+            name: "Babel",
+            subpages: [
+              { name: "Babel Notes", path: "/tools/packaging/babel" },
+              { name: "Babel Code Notes", path: "/tools/packaging/babel/babelcode" },
+            ],
+          },
+          {
+            name: "Webpack",
+            subpages: [
+              { name: "Webpack Notes", path: "/tools/packaging/webpack" },
+              { name: "Webpack Code Notes", path: "/tools/packaging/webpack/webpackcode" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Containerization",
+        subpages: [
+          {
+            name: "Docker",
+            subpages: [
+              { name: "Docker Notes", path: "/tools/containerization/docker" },
+              { name: "Docker Code Notes", path: "/tools/containerization/docker/dockercode" },
+            ],
+          },
         ],
       },
       {
@@ -243,37 +491,45 @@ const pages: Page[] = [
           { name: "Git Code Notes", path: "/git/gitcode" },
         ],
       },
+    ],
+  },
+  {
+    name: "Testing",
+    subpages: [
       {
-        name: "Testing",
+        name: "End-to-End Testing",
         subpages: [
-          {
-            name: "Testing",
-            subpages: [
-              { name: "Testing Notes", path: "/testing" },
-              { name: "Testing Code Notes", path: "/testing/testingcode" },
-            ],
-          },
           {
             name: "Cypress Testing",
             subpages: [
-              {
-                name: "Cypress Testing Notes",
-                path: "/testing/cypresstesting",
-              },
-              {
-                name: "Cypress Testing Code Notes",
-                path: "/testing/cypresstestingcode",
-              },
+              { name: "Cypress Testing Notes", path: "/testing/e2e/cypress" },
+              { name: "Cypress Testing Code Notes", path: "/testing/e2e/cypresscode" },
             ],
           },
           {
-            name: "Vitest Testing",
+            name: "Playwright",
             subpages: [
-              { name: "Vitest Testing Notes", path: "/testing/vitesttesting" },
-              {
-                name: "Vitest Testing Code Notes",
-                path: "/testing/vitesttestingcode",
-              },
+              { name: "Playwright Notes", path: "/testing/e2e/playwright" },
+              { name: "Playwright Code Notes", path: "/testing/e2e/playwrightcode" },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Framework Testing",
+        subpages: [
+          {
+            name: "Jest",
+            subpages: [
+              { name: "Jest Notes", path: "/testing/framework/jest" },
+              { name: "Jest Code Notes", path: "/testing/framework/jestcode" },
+            ],
+          },
+          {
+            name: "Vitest",
+            subpages: [
+              { name: "Vitest Notes", path: "/testing/framework/vitest" },
+              { name: "Vitest Code Notes", path: "/testing/framework/vitestcode" },
             ],
           },
         ],
@@ -291,32 +547,17 @@ const pages: Page[] = [
         ],
       },
       {
+        name: "Rust",
+        subpages: [
+          { name: "Rust Notes", path: "/rust" },
+          { name: "Rust Code Notes", path: "/rust/rustcode" },
+        ],
+      },
+      {
         name: "TypeScript",
         subpages: [
           { name: "TypeScript Notes", path: "/typescript" },
           { name: "TypeScript Code Notes", path: "/typescript/typescriptcode" },
-        ],
-      },
-    ],
-  },
-  {
-    name: "GraphQL & Apollo",
-    subpages: [
-      {
-        name: "GraphQL",
-        subpages: [
-          { name: "GraphQL Notes", path: "/graphql" },
-          { name: "GraphQL Code Notes", path: "/graphql/graphqlcode" },
-        ],
-      },
-      {
-        name: "Apollo Server",
-        subpages: [
-          { name: "Apollo Server Notes", path: "/apolloserver" },
-          {
-            name: "Apollo Server Code Notes",
-            path: "/apolloserver/apolloservercode",
-          },
         ],
       },
     ],
