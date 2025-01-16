9eeae0921408f63ad352ae654558c7c2bc7b1962
Justin <j.hebenstreit25@gmail.com>
2025-01-16 16:23:04 -0600

    finished adding the other to pages to new Pages folder

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import App from "@/App";
-import ErrorPage from "@/pages/Error";
-import Home from "@/pages/Home";
+import ErrorPage from "Pages/Error";
+import Home from "Pages/Home";
 
 // APIs
 import APIs from "Pages/BackEndPages/APIPages/API";
