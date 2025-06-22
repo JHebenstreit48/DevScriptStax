56beedb926deba718f8f8b80fb57a57ad61e2fb9
Justin <j.hebenstreit25@gmail.com>
2025-06-22 16:38:35 -0500

    updated package.json, package-lock.json and name of project in places

--- src/Pages/Special/EntryScreen.tsx
+++ src/Pages/Special/EntryScreen.tsx
@@ -18,7 +18,7 @@ const EntryScreen: React.FC<IntroSplashScreenProps> = ({ onEnter }) => {
   return (
     <div className={`SplashScreen ${isFading ? "fade-out" : ""}`}>
       <img src="/Favicon/CodeQuest.webp" alt="CodeQuest Logo" className="logo" />
-      <h1 className="splash-title">Welcome to CodeQuest</h1>
+      <h1 className="splash-title">Welcome to DevScriptStax</h1>
       <p className="splash-desc">Your journey into coding mastery begins here.</p>
       <button className="splash-button" onClick={handleClick}>
         Enter
