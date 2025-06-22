f72eea36f6ab65de6306c2034dbbd9cdad4c2a36
Justin <j.hebenstreit25@gmail.com>
2025-06-22 13:03:04 -0500

    updated TypeScript and some other navigation files

--- src/Navigation/CombinedNav/CombinedNavTopics/webLanguagesNavCombined.ts
+++ src/Navigation/CombinedNav/CombinedNavTopics/webLanguagesNavCombined.ts
@@ -1,8 +1,8 @@
 import { Page } from '@/Navigation/CombinedNav/CombinedNavAndTypes/NavigationTypes';
 
 import PythonNav from '@/Navigation/IndividualNav/ProgrammingLanguages/Python';
+import TSNav from '@/Navigation/IndividualNav/ProgrammingLanguages/TypeScript';
 import RustNav from '@/Navigation/IndividualNav/ProgrammingLanguages/Rust';
-import TSNav from '@/Navigation/IndividualNav/ProgrammingLanguages/Typescript';
 import GoNav from '@/Navigation/IndividualNav/ProgrammingLanguages/Go';
 import DartNav from '@/Navigation/IndividualNav/ProgrammingLanguages/Dart';
 
@@ -10,8 +10,8 @@ const webLanguagesNav: Page = {
   name: 'Web Languages',
   subpages: [
     PythonNav,
-    RustNav,
     TSNav,
+    RustNav,
     GoNav,
     DartNav
   ]
