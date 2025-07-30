872a033db1afdff56968a608222b8d78e03138c2
Justin <j.hebenstreit25@gmail.com>
2025-07-30 14:00:46 -0500

    updated Angular Advanced Routes

--- src/routes/IndividualRoutes/Granularized/FrontEndRoutes/AngularRoutes/Advanced.tsx
+++ src/routes/IndividualRoutes/Granularized/FrontEndRoutes/AngularRoutes/Advanced.tsx
@@ -2,12 +2,15 @@ import { RouteObject } from "react-router-dom";
 
 import changeDetection from "@/routes/IndividualRoutes/Granularized/FrontEndRoutes/AngularRoutes/Advanced/ChangeDetection";
 import performance from "@/routes/IndividualRoutes/Granularized/FrontEndRoutes/AngularRoutes/Advanced/Performance";
-import RxJSAndReactive from "@/routes/IndividualRoutes/Granularized/FrontEndRoutes/AngularRoutes/Advanced/RxJSAndReactive";
+import rxJSAndReactive from "@/routes/IndividualRoutes/Granularized/FrontEndRoutes/AngularRoutes/Advanced/RxJSAndReactive";
 
 const angularAdvanced: RouteObject[] = [
   ...changeDetection,
   ...performance,
-  ...RxJSAndReactive,
+  ...rxJSAndReactive,
+  // ...animations,
+  // ...typeScriptIntegration,
+  // ...security
 ];
 
 export default angularAdvanced;
