522a0a8719c9c7424e698e06c27bd86513e4b425
Justin <j.hebenstreit25@gmail.com>
2025-07-30 21:01:46 -0500

    fully granularized Angular Advanced Navigation

--- src/Navigation/IndividualNav/Granularized/FrontEndNav/AngularNav/Advanced.ts
+++ src/Navigation/IndividualNav/Granularized/FrontEndNav/AngularNav/Advanced.ts
@@ -3,6 +3,8 @@ import ChangeDetection from "@/Navigation/IndividualNav/Granularized/FrontEndNav
 import Performance from "@/Navigation/IndividualNav/Granularized/FrontEndNav/AngularNav/Advanced/Performance";
 import RxJSAndReactive from "@/Navigation/IndividualNav/Granularized/FrontEndNav/AngularNav/Advanced/RxJSAndReactiveProgram";
 import Animations from "@/Navigation/IndividualNav/Granularized/FrontEndNav/AngularNav/Advanced/Animations";
+import TSIntegration from "@/Navigation/IndividualNav/Granularized/FrontEndNav/AngularNav/Advanced/TSIntegration";
+import Security from "@/Navigation/IndividualNav/Granularized/FrontEndNav/AngularNav/Advanced/SecurityNav";
 
 const AngularAdvanced: Subpage = {
   name: "Advanced",
@@ -11,49 +13,8 @@ const AngularAdvanced: Subpage = {
     Performance,
     RxJSAndReactive,
     Animations,
-    {
-      name: "TypeScript Integration",
-      subpages: [
-        {
-          name: "Path Aliases",
-          path: "/angular/advanced/tsintegration/path-aliases",
-        },
-        {
-          name: "Advanced Aliases",
-          path: "/angular/advanced/tsintegration/advanced-aliases",
-        },
-      ],
-    },
-    {
-      name: "Security",
-      subpages: [
-        {
-          name: "Basics",
-          path: "/angular/advanced/security/overview",
-        },
-        {
-          name: "XSS Protection",
-          path: "/angular/advanced/security/xss",
-        },
-        {
-          name: "CSRF Protection",
-          path: "/angular/advanced/security/csrf",
-        },
-        {
-          name: "Authentication & Authorization",
-          subpages: [
-            {
-              name: "Implementing Authentication",
-              path: "/angular/security/authentication",
-            }, // JWT, OAuth
-            {
-              name: "Role-Based Access Control (RBAC)",
-              path: "/angular/security/rbac",
-            }, // Guards, interceptors
-          ],
-        },
-      ],
-    },
+    TSIntegration,
+    Security,
   ],
 };
 
---
+++ src/Navigation/IndividualNav/Granularized/FrontEndNav/AngularNav/Advanced/TSIntegration.ts
@@ -0,0 +1,17 @@
+import { Subpage } from "@/Navigation/CombinedNav/CombinedNavAndTypes/NavigationTypes";
+
+const TSIntegration: Subpage = {
+  name: "TypeScript Integration",
+  subpages: [
+    {
+      name: "Path Aliases",
+      path: "/angular/advanced/tsintegration/path-aliases",
+    },
+    {
+      name: "Advanced Aliases",
+      path: "/angular/advanced/tsintegration/advanced-aliases",
+    },
+  ],
+};
+
+export default TSIntegration;
