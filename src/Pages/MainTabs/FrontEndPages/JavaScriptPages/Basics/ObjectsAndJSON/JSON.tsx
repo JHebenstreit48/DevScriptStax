5cfcaf6630b4e62ceae1b241d65f296fdb53749f
Justin <j.hebenstreit25@gmail.com>
2025-08-14 21:03:43 -0500

    updated more pages, routes, and navigation

--- src/Pages/MainTabs/FrontEndPages/JavaScriptPages/Basics/ObjectsAndJSON/JSON.tsx
+++ src/Pages/MainTabs/FrontEndPages/JavaScriptPages/Basics/ObjectsAndJSON/JSON.tsx
@@ -1,5 +1,6 @@
 import PageLayout from '@/Components/NavigationUI/PageLayout';
 import Header from "@/Components/Shared/Header/Header";
+import PageTitle from '@/Components/PageComponents/PageTitle';
 import Notes from '@/Components/PageComponents/Notes/Notes';
 
 const JSON = () => {
@@ -9,7 +10,8 @@ const JSON = () => {
   return (
     <>
       <PageLayout>
-        <Header text="JSON" />
+        <Header />
+        <PageTitle title="Working with JSON" />
         <Notes
           filePath={markdownFilePath}
           
