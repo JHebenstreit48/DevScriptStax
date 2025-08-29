ce8961c8403fa580ef27a21890336dff5317cae0
Justin <j.hebenstreit25@gmail.com>
2025-08-29 17:22:00 -0500

    updated Back End and APIs navigation

---
+++ src/Navigation/IndividualNav/FullTopics/BackEndNav/APIsNav/APIs.ts
@@ -0,0 +1,10 @@
+import { Subpage } from '@/Navigation/CombinedNav/CombinedNavAndTypes/NavigationTypes';
+
+const APIs: Subpage = {
+    name: "APIs",
+    subpages: [
+
+    ]
+};
+
+export default APIs;
---
+++ src/Navigation/IndividualNav/FullTopics/BackEndNav/WebServers/Nginx.ts
@@ -0,0 +1,10 @@
+import { Subpage } from '@/Navigation/CombinedNav/CombinedNavAndTypes/NavigationTypes';
+
+const Nginx: Subpage = {
+    name: "Nginx",
+    subpages: [
+
+    ]
+};
+
+export default Nginx;
