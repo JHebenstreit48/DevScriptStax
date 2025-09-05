a4f688991d4526a580cfc42a258361a826020eab
Justin <j.hebenstreit25@gmail.com>
2025-09-05 18:10:08 -0500

    prepped to update Django nav and fixed TS deployment error

--- src/Navigation/IndividualNav/FullTopics/LanguagesNav/TypeScript.ts
+++ src/Navigation/IndividualNav/FullTopics/LanguagesNav/TypeScript.ts
@@ -3,7 +3,6 @@ import { Subpage } from '@/Navigation/CombinedNav/CombinedNavAndTypes/Navigation
 import TSBasics from '@/Navigation/IndividualNav/Granularized/LanguagesNav/TypeScriptNav/Basics';
 import TSIntermediate from '@/Navigation/IndividualNav/Granularized/LanguagesNav/TypeScriptNav/Intermediate';
 import TSAdvanced from '@/Navigation/IndividualNav/Granularized/LanguagesNav/TypeScriptNav/Advanced';
-import TSBestPractices from '@/Navigation/IndividualNav/Granularized/LanguagesNav/TypeScriptNav/BestPractices';
 
 const TSNav: Subpage = {
   name: 'TypeScript',
@@ -11,7 +10,6 @@ const TSNav: Subpage = {
     TSBasics,
     TSIntermediate,
     TSAdvanced,
-    TSBestPractices
   ]
 };
 
