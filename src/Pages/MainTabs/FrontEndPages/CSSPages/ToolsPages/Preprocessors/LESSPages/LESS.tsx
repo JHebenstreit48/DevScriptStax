0bf94c3d96aeb1d077d40dfa5af3502c7678aadc
Justin <j.hebenstreit25@gmail.com>
2025-09-08 09:48:13 -0500

    preparing to remove old LESSPages folder

--- src/Pages/MainTabs/FrontEndPages/CSSPages/ToolsPages/Preprocessors/LESSPages/LESS.tsx
+++ src/Pages/MainTabs/FrontEndPages/CSSPages/ToolsPages/Preprocessors/LESSPages/LESS.tsx
@@ -2,7 +2,7 @@ import Header from "@/Components/Shared/Header/Header";
 import Notes from '@/Components/PageComponents/Notes/Notes';
 
 const LESS = () => {
-    const markdownFilePath = '/LESSNotes/LESS';
+    const markdownFilePath = 'FrontEndNotes/CSSNotes//LessNotes/ToolsNotes/Preprocessors/Less';
 
     return (
 
