aa1f3bd26c2dd330cc0799a329b74b1f4b6cbacc
Justin <j.hebenstreit25@gmail.com>
2025-09-09 18:15:06 -0500

    removed settings.json file, .vscode folder, and granularized Next.js nav

--- src/Navigation/IndividualNav/Granularized/FrontEnd/NextJS/Basics.ts
+++ src/Navigation/IndividualNav/Granularized/FrontEnd/NextJS/Basics.ts
@@ -1,51 +1,14 @@
 import { Subpage } from '@/Navigation/CombinedNav/CombinedNavAndTypes/NavigationTypes';
 
-const NextJSBasics: Subpage = {
+import Fundamentals from '@/Navigation/IndividualNav/Granularized/FrontEnd/NextJS/Basics/Fundamentals';
+import CoreConcepts from '@/Navigation/IndividualNav/Granularized/FrontEnd/NextJS/Basics/CoreConcepts';
+
+const Basics: Subpage = {
   name: 'Basics',
   subpages: [
-    {
-      name: 'Fundamentals',
-      subpages: [
-        {
-          name: 'Getting Started',
-          path: '/nextjs/basics/fundamentals/getting-started'
-        },
-        {
-          name: 'Project Structure',
-          path: '/nextjs/basics/fundamentals/project-structure'
-        },
-        {
-          name: 'Routing',
-          path: '/nextjs/basics/fundamentals/routing'
-        },
-        {
-          name: 'API Routes',
-          path: '/nextjs/basics/fundamentals/api-routes'
-        }
-      ]
-    },
-    {
-      name: 'Core Concepts',
-      subpages: [
-        {
-          name: 'Static Generation (SSG)',
-          path: '/nextjs/core-concepts/static-generation'
-        },
-        {
-          name: 'Server-Side Rendering (SSR)',
-          path: '/nextjs/core-concepts/server-side-rendering'
-        },
-        {
-          name: 'Client-Side Rendering (CSR)',
-          path: '/nextjs/core-concepts/client-side-rendering'
-        },
-        {
-          name: 'Middleware',
-          path: '/nextjs/core-concepts/middleware'
-        }
-      ]
-    }
+    Fundamentals,
+    CoreConcepts
   ]
 };
 
-export default NextJSBasics;
+export default Basics;
