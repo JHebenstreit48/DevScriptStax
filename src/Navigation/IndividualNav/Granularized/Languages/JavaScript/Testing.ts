2f78258eab1498802c492f45f15c181f2ccc1c04
Justin <j.hebenstreit25@gmail.com>
2025-09-15 20:40:55 -0500

    updated JS > Testing navigation

--- src/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing.ts
+++ src/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing.ts
@@ -1,14 +1,45 @@
 import { Subpage } from "@/Navigation/CombinedNav/CombinedNavAndTypes/NavigationTypes";
 
-import Fundamentals from '@/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing/Fundamentals';
-import Tools from '@/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing/Tools';
+// ------------------------------------------------------- Basics Start -------------------------------------------------------
+import Fundamentals from '@/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing/Basics/Fundamentals';
+import Authoring from '@/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing/Basics/Authoring';
+import DoublesAndIsolation from '@/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing/Basics/DoublesAndIsolation';
+import AsyncAndTimers from '@/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing/Basics/AsyncAndTimers';
+import ModulesAndState from '@/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing/Basics/ModulesAndState';
+// ------------------------------------------------------- Basics End ---------------------------------------------------------
+
+// ------------------------------------------------------- Advanced Start -------------------------------------------------------
+import StabilityAndData from '@/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing/Advanced/StabilityAndData';
+import Environment from '@/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing/Advanced/Environment';
+import CoverageAndQuality from '@/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing/Advanced/CoverageAndQuality';
+import Runners from '@/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing/Advanced/Runners';
+import CIAndDebug from '@/Navigation/IndividualNav/Granularized/Languages/JavaScript/Testing/Advanced/CIAndDebug';
+// ------------------------------------------------------- Advanced End ---------------------------------------------------------
 
 const Testing: Subpage = {
   name: "Testing",
   subpages: [
-    Fundamentals,
-    Tools
-  ],
+    {
+      name: "Basics",
+      subpages: [
+        Fundamentals,
+        Authoring,
+        DoublesAndIsolation,
+        AsyncAndTimers,
+        ModulesAndState
+      ]
+    },
+    {
+      name: "Advanced",
+      subpages: [
+        StabilityAndData,
+        Environment,
+        CoverageAndQuality,
+        Runners,
+        CIAndDebug
+      ]
+    }
+  ]
 };
 
 export default Testing;
