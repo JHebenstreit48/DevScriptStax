18a08606327e22419139cf3a88a669d1d692d308
Justin <j.hebenstreit25@gmail.com>
2025-10-26 16:45:23 -0500

    updated page files markdown const that caused deployment issues

--- src/Pages/MainTabs/FrontEnd/React/Basics/Fundamentals/Introduction.tsx
+++ src/Pages/MainTabs/FrontEnd/React/Basics/Fundamentals/Introduction.tsx
@@ -1,11 +1,10 @@
-import PageLayout from "@/Components/NavigationUI/PageLayout";
-import Header from "@/Components/Shared/Header/Header";
-import PageTitle from "@/Components/PageComponents/PageTitle";
-import Notes from "@/Components/PageComponents/Notes/Notes";
+import PageLayout from '@/Components/NavigationUI/PageLayout';
+import Header from '@/Components/Shared/Header/Header';
+import PageTitle from '@/Components/PageComponents/PageTitle';
+import Notes from '@/Components/PageComponents/Notes/Notes';
 
 const ReactIntro = () => {
-  const markdownFilePath =
-    'FrontEnd/ReactNotes/Basics/Fundamentals/Introduction";
+  const markdownFilePath = 'FrontEnd/ReactNotes/Basics/Fundamentals/Introduction';
 
   return (
     <>
