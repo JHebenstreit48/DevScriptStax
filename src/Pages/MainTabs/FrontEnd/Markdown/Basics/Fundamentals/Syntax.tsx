873e65bb4c3d99617162491e9e95ba2f2b009d04
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-01-05 20:57:03 -0600

    updated pages files for incorrect file path

--- src/Pages/MainTabs/FrontEnd/Markdown/Basics/Fundamentals/Syntax.tsx
+++ src/Pages/MainTabs/FrontEnd/Markdown/Basics/Fundamentals/Syntax.tsx
@@ -4,7 +4,7 @@ import PageTitle from '@/Components/PageComponents/PageTitle';
 import Notes from '@/Components/PageComponents/Notes/Notes';
 
 const Syntax = () => {
-  const markdownFilePath = 'FrontEnd/Markdown/Basics/Fundamentals/Fundamentals/Syntax';
+  const markdownFilePath = 'FrontEnd/Markdown/Basics/Fundamentals/Syntax';
 
   return (
     <>
