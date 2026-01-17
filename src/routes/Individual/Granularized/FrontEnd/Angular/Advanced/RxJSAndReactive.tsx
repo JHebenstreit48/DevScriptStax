52e3064af93e71f45275b496d7728bc5f9c2b53c
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-01-17 13:47:46 -0600

    updating Navigation SCSS

--- src/routes/Individual/Granularized/FrontEnd/Angular/Advanced/RxJSAndReactive.tsx
+++ src/routes/Individual/Granularized/FrontEnd/Angular/Advanced/RxJSAndReactive.tsx
@@ -4,11 +4,23 @@ import { RouteObject } from 'react-router-dom';
 const ObservableBasics = lazy(
   () => import('@/Pages/MainTabs/FrontEnd/Angular/Advanced/RxJSAndReactive/ObservableBasics')
 );
+const SubjectsAndBehavior = lazy(
+  () => import('@/Pages/MainTabs/FrontEnd/Angular/Advanced/RxJSAndReactive/SubjectsAndBehavior')
+);
+const RxJSOperators = lazy(() => import('@/Pages/MainTabs/FrontEnd/Angular/Advanced/RxJSAndReactive/RxJSOperators'));
 
 const rxJSAndReactive: RouteObject[] = [
   {
     path: '/angular/advanced/rxjs/observables',
     element: <ObservableBasics />,
+  },
+  {
+    path: '/angular/advanced/rxjs/subjects',
+    element: <SubjectsAndBehavior />,
+  },
+  {
+    path: '/angular/advanced/rxjs/operators',
+    element: <RxJSOperators />,
   },
 ];
 
