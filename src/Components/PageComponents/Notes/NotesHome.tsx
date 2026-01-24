b299b240a35fbd404aaa45cc566bcb5ff22c560e
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-01-24 17:47:44 -0600

    removed some unnecessary files and updated package.json

--- src/Components/PageComponents/Notes/NotesHome.tsx
+++
@@ -1,8 +0,0 @@
-import { useMemo } from "react";
-import homeMd from "@/content/Home.md?raw";
-import HomeRenderer from "@/Components/PageComponents/Notes/HomeText/HomeRenderer";
-
-export default function NotesHome() {
-  const content = useMemo(() => homeMd, []);
-  return <HomeRenderer content={content} />;
-}
