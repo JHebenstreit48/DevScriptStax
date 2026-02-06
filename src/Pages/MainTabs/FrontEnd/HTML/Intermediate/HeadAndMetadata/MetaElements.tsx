778ebda9ed4cb000540f4f5c1a020443184b58eb
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-02-05 23:54:11 -0600

    updated HTML page for meta tags

--- src/Pages/MainTabs/FrontEnd/HTML/Intermediate/HeadAndMetadata/MetaElements.tsx
+++ src/Pages/MainTabs/FrontEnd/HTML/Intermediate/HeadAndMetadata/MetaElements.tsx
@@ -4,7 +4,7 @@ import PageTitle from '@/Components/PageComponents/PageTitle';
 import Notes from '@/Components/PageComponents/Notes/Notes';
 
 const MetaElements = () => {
-  const markdownFilePath = 'FrontEnd/HTML/Intermediate/HeadAndMetadata/MetaElements';
+  const markdownFilePath = 'FrontEnd/HTML/Intermediate/HeadAndMetadata/Meta';
 
   return (
     <>
