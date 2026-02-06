0be90019e692397b6eb73b49cdf2525633d6e0ad
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-02-06 12:12:26 -0600

    updated markdown file path for Node notes

--- src/Pages/MainTabs/BackEnd/Frameworks/NodeJs/Basics/Fundamentals/EventLoopAndAsyncIO.tsx
+++ src/Pages/MainTabs/BackEnd/Frameworks/NodeJs/Basics/Fundamentals/EventLoopAndAsyncIO.tsx
@@ -4,7 +4,7 @@ import PageTitle from '@/Components/PageComponents/PageTitle';
 import Notes from '@/Components/PageComponents/Notes/Notes';
 
 const EventLoopAndAsyncIO = () => {
-  const markdownFilePath = 'BackEnd/Frameworks/Node/Basics/Fundamentals/EventLoopAsyncIO';
+  const markdownFilePath = 'BackEnd/Frameworks/NodeJs/Basics/Fundamentals/EventLoopAsyncIO';
 
   return (
     <>
